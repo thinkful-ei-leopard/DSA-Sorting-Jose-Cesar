7197c44939948086cb3b77d1e59f5d6a26acd043
Jose <jozylopez27@gmail.com>
2020-04-09 15:12:29 -0500

    drill 2

--- sorting.js
+++ sorting.js
@@ -25,8 +25,58 @@ function mergeSort(array) {
 // third recursive call:
 // 21, 1 | 26, 45
 
+// part 2
+// list to be sorted : 9
 
 // part 3
 // first lists to be sorted: 21 | 1 => 1,21
+
+// part 4
 // two lists to be merged on 7th  merge:
 //1, 21, 26, 45 | 2, 9, 28, 29
+
+
+//drill 2 
+
+function partition(array, start, end) {
+    const pivot = array[end - 1];
+    let j = start;
+    for (let i = start; i < end - 1; i++) {
+        if (array[i] <= pivot) {
+            swap(array, i, j);
+            j++;
+        }
+    }
+    swap(array, end-1, j);
+    return j;
+};
+
+function quickSort(array, start = 0, end = array.length) {
+    if (start >= end) {
+        return array;
+    }
+    const middle = partition(array, start, end);
+    array = quickSort(array, start, middle);
+    array = quickSort(array, middle + 1, end);
+    return array;
+};
+
+
+
+// part 1
+// You partition the array into two halves around a pivot value.
+// All the values which are less than the pivot value go to one half of the array
+// and all of the values which are greater than the pivot go to the other half of 
+// the array.
+
+// Answer: 
+// The pivot could have been either 14 or 17 because all of the values right of 14 are greater than
+//14, and all the values left of 14 are smaller than 14. same goes for 17.
+// 3 9 1 < 14 > < 17 > 24 22 20
+
+
+// part 2
+// [3, 9, 10, 12, 19, 14, 17, 16, 13, 15]
+// [14, 13, 10, 3, 9, 12, 15, 16, 19, 17]
+
+// drill 3
